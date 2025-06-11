046915cda98a2b2f35502d35e4e0fcb245b993c0
aika <ercamanalo@gmail.com>
2025-06-11 15:36:33 -0700

    java methods

--- src/main/java/methods/GreetUser.java
+++ src/main/java/methods/GreetUser.java
@@ -1,4 +1,29 @@
 package methods;
+import java.util.Scanner;
 
 public class GreetUser {
+    /*Write a method that asks a user for their name, then another method that greets the user by name*/
+    public static void main(String[] args) {
+        // we're using the main method as a control mechanism
+        // instead of doing everything inside this 'main' method, make calls to methods
+        String name = getUserName();
+        greetUser(name);
+    }
+
+    // returns a value of String (which is the user's name)
+    public static String getUserName() {
+        System.out.println("Enter your name");
+        Scanner scanner = new Scanner(System.in);
+
+        // note that even if line 8 and line 17 share the same variable 'name', it is scoped inside the method it was declared in
+        String name = scanner.next();
+        scanner.close();
+
+        return name;
+    }
+
+    // greetUser method doesn't return anything, just prints out a greeting
+    public static void greetUser(String name) {
+        System.out.println("Hello " + name + "!");
+    }
 }
---
+++ src/main/java/methods/InstantCreditCheck.java
@@ -0,0 +1,55 @@
+package methods;
+import java.util.Scanner;
+
+public class InstantCreditCheck {
+    /*Write a program that approves anyone who:
+        - makes at least $25,000
+        - has a credit score of 700 or better
+    Reject all others...*/
+
+    // you can define variables at a class level
+    // the scope of these variables are accessible to all methods define inside this class
+    static double requiredSalary = 25000;
+    static int requiredCreditScore = 700;
+
+    // move scanner at a higher level (class level in this case) to give access to other methods
+    // added static keyword because static methods can only access other static modified stuff
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // we're going to use the main method to control the flow of the program
+        double salary = getSalary();
+        int creditScore = getCreditScore();
+
+        //after getting the return values of our methods, we can close the scanner here...
+        scanner.close();
+
+        boolean isQualified = isUserQualified(salary, creditScore);
+        notifyUser(isQualified);
+    }
+
+    public static double getSalary() {
+        System.out.println("What is your salary?");
+        // dont need int salary = scanner.nextDouble(); return salary. it's redundant
+        return scanner.nextDouble();
+    }
+
+    public static int getCreditScore() {
+        System.out.println("What is your credit score?");
+        // same for redundancy here
+        return scanner.nextInt();
+    }
+
+    public static boolean isUserQualified(double salary, int creditScore) {
+        // can simplify the same as JavaScript one-liners
+        return salary >= requiredSalary && creditScore >= requiredCreditScore;
+    }
+
+    public static void notifyUser(boolean isQualified) {
+        if (isQualified)
+            System.out.println("Congratulations! You have been approved.");
+        else {
+            System.out.println("Sorry, you do not qualify.");
+        }
+    }
+}
