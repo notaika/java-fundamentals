0c6e86a2109424c692c4404e56d6229a87cd05b1
aika <ercamanalo@gmail.com>
2025-06-12 15:14:49 -0700

    java string methods practice

---
+++ src/main/java/practice/StringPractice.java
@@ -0,0 +1,73 @@
+package practice;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StringPractice {
+    /*Practicing + experimenting with new string methods*/
+    public static void main(String[] args) {
+        System.out.println("Enter word to be analyzed:");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        System.out.println("Enter the character of word to be analyzed:");
+        String c = scanner.next();
+        scanner.close();
+
+
+        int strLength = getStrLength(word);
+        System.out.println("String length: " + strLength);
+
+        List<Character> strChars = getCharAt(word);
+        System.out.println("String chars: " + strChars);
+
+        boolean isStrEqual = isStrEqual(word);
+        System.out.println("String equals hello?: " + isStrEqual);
+
+        int indexOfChar = indexOfChar(c, word);
+        System.out.println("String index of " + c + ": " + indexOfChar);
+
+        String capitalized = capitalizedWord(word);
+        System.out.println("Capitalized word: " + capitalized);
+
+        String lowercased = lowercasedWord(word);
+        System.out.println("Lowercased word: " + lowercased);
+
+        String replacedStr = replaceChar(c, word);
+        System.out.println("Replaced char: " + replacedStr);
+
+        // the original string is immutable; pass-by-value
+        System.out.println("Strings are immutable; original word: " + word);
+    }
+
+    public static int getStrLength(String str) {
+        return str.length();
+    }
+
+    public static List<Character> getCharAt(String str) {
+        List<Character> chars = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            chars.add(str.charAt(i));
+        }
+        return chars;
+    }
+
+    public static boolean isStrEqual(String str) {
+        return str.equals("hello");
+    }
+
+    public static int indexOfChar(String c, String str) {
+        return str.indexOf(c);
+    }
+
+    public static String capitalizedWord(String str) {
+        return str.toUpperCase();
+    }
+
+    public static String lowercasedWord(String str) {
+        return str.toLowerCase();
+    }
+
+    public static String replaceChar(String c, String str){
+        return str.replace(c, str);
+    }
+}
