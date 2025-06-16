a07da7ad46207418b0b910df92b39f09870870c6
aika <ercamanalo@gmail.com>
2025-06-16 14:03:02 -0700

    java type casting, added examples of upcast and downcast

--- src/main/java/fundamentals/polymorphism/Main.java
+++ src/main/java/fundamentals/polymorphism/Main.java
@@ -7,18 +7,26 @@ public class Main {
         goldie.makeSound();
         goldie.fetch();
 
-        // Runtime polymorphism
+        // Runtime polymorphism + implicit upcast
         Animal dog = new Dog();
         dog.makeSound();
-//        dog.fetch(); does not work, because 'fetch' method is not defined in Animal
+//        dog.fetch(); does not work, because 'fetch' method is not defined in Animal (implicit upcast)
 
         Cat persian = new Cat();
         persian.makeSound();
         persian.scratch();
 
-        // Runtime polymorphism
+        // Runtime polymorphism + implicit upcast
         Animal cat = new Cat();
         cat.makeSound();
-//        cat.scratch(); does not work, because 'scratch' method is not defined in Animal
+//        cat.scratch(); does not work, because 'scratch' method is not defined in Animal (implicit upcast)
+
+        // ============ Explicit Type Casting Examples start here ==============
+        // Downcast 'dog' to play fetch
+        ((Dog) dog).fetch(); // dog.fetch(); explicitly on Animal; this is a temporary cast to provide access
+
+        // Downcast 'cat' to scratch
+        Cat notACat = (Cat) cat;
+        notACat.scratch();
     }
 }
